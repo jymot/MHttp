4906b43498c600a6133327957686ffd3e09ef671
wangchao <magician.of.technique@gmail.com>
2015-11-19 11:35:44 +0800

    (*) commit FileResponseHandler,  append callback onSuccess(File file, HttpResponse response);

--- library/src/main/java/im/wangchao/mhttp/FileResponseHandler.java
+++ library/src/main/java/im/wangchao/mhttp/FileResponseHandler.java
@@ -1,6 +1,10 @@
 package im.wangchao.mhttp;
 
+import android.content.Context;
 import android.support.annotation.Nullable;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * <p>Description  : FileResponseHandler.</p>
@@ -10,16 +14,39 @@ import android.support.annotation.Nullable;
  * <p>Time         : 下午2:39.</p>
  */
 public class FileResponseHandler extends AbsResponseHandler {
-    @Override protected void onSuccess(HttpResponse response) {
+    final private File file;
 
+    public FileResponseHandler(Context context){
+        this.file = getTempFile(context);
+    }
+
+    public FileResponseHandler(File file){
+        this.file = file;
+    }
+
+    @Override final protected void onSuccess(HttpResponse response) {
+        onSuccess(file, response);
     }
 
     @Override protected void onFailure(HttpResponse response, @Nullable Throwable throwable) {
 
     }
 
-    @Override
-    public ResponseDataType getResponseDataType() {
+    @Override public ResponseDataType getResponseDataType() {
+        ResponseDataType dataType = ResponseDataType.FILE;
+        dataType.set(file);
         return ResponseDataType.FILE;
+    }
+
+    public void onSuccess(File file, HttpResponse response){
+
+    }
+
+    private File getTempFile(Context context){
+        try {
+            return File.createTempFile("temp_", "_handled", context.getCacheDir());
+        } catch (IOException e) {
+            return null;
+        }
     }
 }
