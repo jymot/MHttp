30711ea36cc1bb5200f693864729ffd7112463c5
wangchao <magician.of.technique@gmail.com>
2015-11-27 17:27:22 +0800

    (*)temp

---
+++ library/src/main/java/im/wangchao/mhttp/HttpClientManager.java
@@ -0,0 +1,12 @@
+package im.wangchao.mhttp;
+
+/**
+ * <p>Description  : HttpClientManager.</p>
+ * <p/>
+ * <p>Author       : wangchao.</p>
+ * <p>Date         : 15/11/27.</p>
+ * <p>Time         : 下午5:25.</p>
+ */
+public class HttpClientManager {
+    // TODO: 15/11/27 manage all request, eg. cancel request , common settings ..
+}
