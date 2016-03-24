917ee3fa22b3aeb2201f65bfe6abe251a95214a8
wangchao <magician.of.technique@gmail.com>
2016-03-24 14:04:16 +0800

    (*)add simple GSONResponseHandler

--- mhttp/src/main/java/im/wangchao/mhttp/GSONResponseHandler.java
+++ mhttp/src/main/java/im/wangchao/mhttp/GSONResponseHandler.java
@@ -1,5 +1,8 @@
 package im.wangchao.mhttp;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 /**
  * <p>Description  : GSONResponseHandler.</p>
  * <p/>
@@ -7,6 +10,23 @@ package im.wangchao.mhttp;
  * <p>Date         : 16/3/20.</p>
  * <p>Time         : 上午9:06.</p>
  */
-public class GSONResponseHandler {
-// TODO: 16/3/22  
+public abstract class GSONResponseHandler<T> extends JSONResponseHandler{
+
+    @Override final public void onSuccess(JSONArray jsonArray, HttpResponse response) {
+        super.onSuccess(jsonArray, response);
+        onSuccess(parser(jsonArray.toString()));
+    }
+
+    @Override final public void onSuccess(JSONObject jsonObject, HttpResponse response) {
+        super.onSuccess(jsonObject, response);
+        onSuccess(parser(jsonObject.toString()));
+    }
+
+    /** parser Json to T */
+    protected abstract T parser(String jsonString);
+
+    public void onSuccess(T t){
+
+    }
+
 }
