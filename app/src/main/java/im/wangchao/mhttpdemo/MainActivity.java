300a039341e54cfd0cf92ed3994d68e48a33cdb1
wangchao <magician.of.technique@gmail.com>
2016-04-13 15:01:20 +0800

    (*)commit sample code

--- app/src/main/java/im/wangchao/mhttpdemo/MainActivity.java
+++ app/src/main/java/im/wangchao/mhttpdemo/MainActivity.java
@@ -7,11 +7,16 @@ import android.util.Log;
 import android.view.View;
 
 import java.io.File;
+import java.io.IOException;
 
 import im.wangchao.mhttp.FileResponseHandler;
+import im.wangchao.mhttp.HttpManager;
 import im.wangchao.mhttp.HttpRequest;
 import im.wangchao.mhttp.HttpResponse;
 import im.wangchao.mhttp.TextResponseHandler;
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
     final static String TAG = MainActivity.class.getSimpleName();
@@ -21,6 +26,42 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        HttpManager.instance().cache(this, "tempcache");
+        HttpManager.instance().addNetworkInterceptor(new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                log("addNetworkInterceptor!");
+                Request request = chain.request();
+                long t1 = System.nanoTime();
+                log(String.format("Sending request %s on %s%n%s",
+                        request.url(), chain.connection(), request.headers()));
+
+                Response response = chain.proceed(request);
+
+                long t2 = System.nanoTime();
+                log(String.format("Received response for %s in %.1fms%n%s",
+                        response.request().url(), (t2 - t1) / 1e6d, response.headers()));
+
+                return response.newBuilder()
+                        .header("Cache-Control", "max-age=60")
+                        .removeHeader("Pragma")
+                        .build();
+            }
+        });
+        HttpManager.instance().addInterceptor(new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request request = chain.request();
+
+                log("addInterceptor!");
+                Response response = chain.proceed(request);
+
+                return response.newBuilder()
+                        .header("Cache-Control", "max-age=60")
+                        .removeHeader("Pragma")
+                        .build();
+            }
+        });
         findViewById(R.id.getFile).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
