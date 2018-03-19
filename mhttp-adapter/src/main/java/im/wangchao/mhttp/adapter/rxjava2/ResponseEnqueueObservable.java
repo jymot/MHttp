8f7e2830c57fd95999bae23277ef4bd0dd9f8177
wangchao <magician.of.technique@gmail.com>
2018-03-19 17:44:35 +0800

    [*] Temp commit to sync

---
+++ mhttp-adapter/src/main/java/im/wangchao/mhttp/adapter/rxjava2/ResponseEnqueueObservable.java
@@ -0,0 +1,10 @@
+package im.wangchao.mhttp.adapter.rxjava2;
+
+/**
+ * <p>Description  : ResponseEnqueueObservable.</p>
+ * <p>Author       : wangchao.</p>
+ * <p>Date         : 2018/3/19.</p>
+ * <p>Time         : 下午4:45.</p>
+ */
+public class ResponseEnqueueObservable {
+}
