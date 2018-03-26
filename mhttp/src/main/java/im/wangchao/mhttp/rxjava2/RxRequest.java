03aab17eacd2b3221a59cffe3cb980b56f08e50a
wangchao <magician.of.technique@gmail.com>
2018-03-26 17:47:54 +0800

    [*] temp commit for sync data

---
+++ mhttp/src/main/java/im/wangchao/mhttp/rxjava2/RxRequest.java
@@ -0,0 +1,16 @@
+package im.wangchao.mhttp.rxjava2;
+
+import im.wangchao.mhttp.Request;
+
+/**
+ * <p>Description  : RxRequest.</p>
+ * <p>Author       : wangchao.</p>
+ * <p>Date         : 2018/3/26.</p>
+ * <p>Time         : 下午5:40.</p>
+ */
+public class RxRequest<T> {
+    private Request request;
+
+    // todo RxRequest type-safe
+
+}
